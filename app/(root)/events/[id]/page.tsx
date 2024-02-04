a5fa06bc45dfdef52e4c6367977dbf4b50491b3d
Ajesh S <ajeshajesh9895@gmail.com>
2024-02-04 13:00:25 +0530

    implemented stripe and cards

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -65,7 +65,7 @@ const EventDetails = async ({
                   width={32}
                   height={32}
                 />
-                <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
+                <div className="p-medium-16 gap-x-2 lg:p-regular-20 flex flex-wrap items-center">
                   <p>
                     {formatDateTime(event.startDateTime).dateOnly} -{' '}
                     {formatDateTime(event.startDateTime).timeOnly}
